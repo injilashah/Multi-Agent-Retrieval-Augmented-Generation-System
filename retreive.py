394a877b0223fb9ee4b6bdc3087146e673a8b893
Injila <110339290+injilashah@users.noreply.github.com>
2025-01-20 15:51:47 +0530

    Update retreive.py

--- retreive.py
+++ retreive.py
@@ -3,16 +3,11 @@ import os
 from sentence_transformers import SentenceTransformer
 from ner import query_parser  # Import the improved query parser
 import pdfplumber
-# Load pre-trained Sentence Transformer model
+# Load  Sentence Transformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
-
 # Load the FAISS index
 index = faiss.read_index("document_index.faiss")
-
-
-
-
-file_path = 'I:/genaii/content/genai/documents'  # Path where your PDF documents are stored
+file_path = 'I:/genaii/content/genai/documents'  # Path wherePDF documents are stored
 documents = []
 doc_paths = []
 
@@ -36,22 +31,19 @@ def retrieve_documents(key_terms, k=3):
     :param k: Number of top results to return
     :return: List of tuples [(document_path, document_snippet, distance)]
     """
-    
-   
     query_embedding = model.encode([key_terms])
 
-    # Step 3: Perform search using FAISS
+    #Perform search using FAISS
     distances, indices = index.search(query_embedding, k)
-
-    # Step 4: Prepare the results
+    #Prepare the results
     results = []
     for i in range(k):
         doc_index = indices[0][i]  # Get the index of the retrieved document
-        document_snippet = documents[doc_index][:]  # Get the first 200 characters as a snippet
+        document_snippet = documents[doc_index][:] 
         document_path = doc_paths[doc_index]  # Retrieve the document path or title
         distance = distances[0][i]  # Similarity score
         distance = round(distance, 2)
-        # Append to the results as a tuple
+        
         results.append((document_path, document_snippet, distance))
 
     return results
